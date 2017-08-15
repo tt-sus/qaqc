0b0bec1ba86e13797e208f9a0c5bd83cda434862
akshar07 <takleakshar@gmail.com>
2017-08-15 12:03:09 -0400

    done

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,7 +10,6 @@ import { Router } from '@angular/router';
 export class AuthService {
   auth: firebase.auth.Auth;
   user: Observable<firebase.User>;
-<<<<<<< HEAD
   userName:string;
   isLoggedIn:boolean;
   constructor(private firebaseAuth: AngularFireAuth,private router:Router, ) {
@@ -32,37 +31,12 @@ export class AuthService {
       });   
      
   }
-=======
-  userName: string;
-  isLoggedIn: boolean;
-  
-  constructor(private firebaseAuth: AngularFireAuth, private router: Router, ) {
-    this.user = firebaseAuth.authState;
-  }
-
-  // signup(email: string, password: string, displayName: string) {
-  //   alert(displayName)
-  //   this.firebaseAuth
-  //     .auth
-  //     .createUserWithEmailAndPassword(email, password)
-  //     .then(user => {
-  //       console.log(user);
-  //       return user.updateProfile({ displayName: displayName })
-
-  //     })
-  //     .catch(err => {
-  //       alert(`'Something went wrong:',${err.message}`);
-  //     });
-
-  // }
->>>>>>> 01abb4ce3bccb9968522feae2cc3ea8a8b42710b
 
   login(email: string, password: string) {
     this.firebaseAuth
       .auth
       .signInWithEmailAndPassword(email, password)
       .then(value => {
-<<<<<<< HEAD
         console.log(value)
         if(this.firebaseAuth.auth){
           this.userName=value.email;
@@ -86,31 +60,6 @@ export class AuthService {
       return true;
     }
     else{
-=======
-        //console.log(value)
-        if (this.firebaseAuth.auth) {
-          this.userName = value.email;
-          if (this.firebaseAuth.auth.currentUser) { this.isLoggedIn = true; }
-
-          this.router.navigate(["home"]);
-
-        }
-        else {
-          alert('Username or Password is not correct!');
-          this.router.navigate([""]);
-        }
-        //console.log(value);
-      })
-      .catch(err => {
-        alert(`'Something went wrong:', ${err.message}`);
-      });
-  }
-  sendToken() {
-    if (this.firebaseAuth.auth.currentUser) {
-      return true;
-    }
-    else {
->>>>>>> 01abb4ce3bccb9968522feae2cc3ea8a8b42710b
       return false;
     }
   }
@@ -120,7 +69,6 @@ export class AuthService {
       .auth
       .signOut();
 
-<<<<<<< HEAD
       this.router.navigate([""]);
   }
       sendUser(){
@@ -134,18 +82,6 @@ export class AuthService {
     this.auth.sendPasswordResetEmail(email).then(function() {
       alert(`email sent to ${email}`)
     }, function(error) {
-=======
-    this.router.navigate([""]);
-  }
-
-  resetPassword(email) {
-    this.auth = firebase.auth();
-
-
-    this.auth.sendPasswordResetEmail(email).then(function () {
-      alert(`email sent to ${email}`)
-    }, function (error) {
->>>>>>> 01abb4ce3bccb9968522feae2cc3ea8a8b42710b
       alert("try again")
     });
   }
