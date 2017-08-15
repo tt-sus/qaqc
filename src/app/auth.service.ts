3a1d350cb87d4fb330bb248a851f526221ee77a9
MingboPeng <mingo1214@gmail.com>
2017-08-15 12:05:22 -0400

    fixed confilex

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,29 +10,8 @@ import { Router } from '@angular/router';
 export class AuthService {
   auth: firebase.auth.Auth;
   user: Observable<firebase.User>;
-<<<<<<< HEAD
-  userName:string;
-  isLoggedIn:boolean;
-  constructor(private firebaseAuth: AngularFireAuth,private router:Router, ) {
-    this.user = firebaseAuth.authState;
-  }
 
-  signup(email: string, password: string , displayName:string) {
-    alert(displayName)
-    this.firebaseAuth
-      .auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(user => {
-        console.log(user);
-        return user.updateProfile({displayName:displayName})
-      
-      })
-      .catch(err => {
-        alert(`'Something went wrong:',${err.message}`);
-      });   
-     
-  }
-=======
+
   userName: string;
   isLoggedIn: boolean;
   
@@ -40,53 +19,29 @@ export class AuthService {
     this.user = firebaseAuth.authState;
   }
 
-  // signup(email: string, password: string, displayName: string) {
-  //   alert(displayName)
-  //   this.firebaseAuth
-  //     .auth
-  //     .createUserWithEmailAndPassword(email, password)
-  //     .then(user => {
-  //       console.log(user);
-  //       return user.updateProfile({ displayName: displayName })
+  signup(email: string, password: string, displayName: string) {
+    alert(displayName)
+    this.firebaseAuth
+      .auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        console.log(user);
+        return user.updateProfile({ displayName: displayName })
 
-  //     })
-  //     .catch(err => {
-  //       alert(`'Something went wrong:',${err.message}`);
-  //     });
+      })
+      .catch(err => {
+        alert(`'Something went wrong:',${err.message}`);
+      });
 
-  // }
->>>>>>> 01abb4ce3bccb9968522feae2cc3ea8a8b42710b
+  }
+
 
   login(email: string, password: string) {
     this.firebaseAuth
       .auth
       .signInWithEmailAndPassword(email, password)
       .then(value => {
-<<<<<<< HEAD
-        console.log(value)
-        if(this.firebaseAuth.auth){
-          this.userName=value.email;
-          if(this.firebaseAuth.auth.currentUser){ this.isLoggedIn=true;}
-         
-           this.router.navigate(["home"]);
-           
-        }
-          else{
-             this.router.navigate([""]);
-          }
-        console.log(value);
-      })
-      .catch(err => {
-        console.log('Something went wrong:',err.message);
-      });
-       this.sendUser()
-  }
-  sendToken(){
-    if(this.firebaseAuth.auth.currentUser){
-      return true;
-    }
-    else{
-=======
+
         //console.log(value)
         if (this.firebaseAuth.auth) {
           this.userName = value.email;
@@ -110,7 +65,7 @@ export class AuthService {
       return true;
     }
     else {
->>>>>>> 01abb4ce3bccb9968522feae2cc3ea8a8b42710b
+
       return false;
     }
   }
@@ -120,21 +75,7 @@ export class AuthService {
       .auth
       .signOut();
 
-<<<<<<< HEAD
-      this.router.navigate([""]);
-  }
-      sendUser(){
-      
-     
-  }
-  resetPassword(email){
-     this.auth = firebase.auth();
 
-
-    this.auth.sendPasswordResetEmail(email).then(function() {
-      alert(`email sent to ${email}`)
-    }, function(error) {
-=======
     this.router.navigate([""]);
   }
 
@@ -145,7 +86,6 @@ export class AuthService {
     this.auth.sendPasswordResetEmail(email).then(function () {
       alert(`email sent to ${email}`)
     }, function (error) {
->>>>>>> 01abb4ce3bccb9968522feae2cc3ea8a8b42710b
       alert("try again")
     });
   }
